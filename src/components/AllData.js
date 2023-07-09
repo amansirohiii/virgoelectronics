8e8ece39137d580ee9a42ced65e771657866bb85
Aman Sirohi <amansirohi077@gmail.com>
2023-07-09 21:36:53 +0530

    added images

--- src/components/AllData.js
+++ src/components/AllData.js
@@ -1,63 +1,7 @@
-// import Img1 from "../img/products/1.jpg";
-// import Img2 from "../img/products/2.jpg";
-// import Img3 from "../img/products/3.png";
-// import Img4 from "../img/products/4.jpg";
-// import Img5 from "../img/products/5.jpg";
-// import Img6 from "../img/products/6.jpg";
-// import Img7 from "../img/products/7.jpg";
-// import Img8 from "../img/products/8.jpg";
-// import Img9 from "../img/products/9.png";
-// import Img10 from "../img/products/10.jpg";
-// import Img11 from "../img/products/11.jpg";
-// import Img12 from "../img/products/12.png";
-// import Img13 from "../img/products/13.jpg";
-// import Img14 from "../img/products/14.png";
-// import Img15 from "../img/products/15.jpg";
-// import Img16 from "../img/products/16.jpg";
-// import Img17 from "../img/products/17.png";
-// import Img18 from "../img/products/18.png";
-// import Img19 from "../img/products/19.jpg";
-// import Img20 from "../img/products/20r.jpg";
-// import OtherImgs1 from "../img/products/otherProducts/other1.jpg";
-// import OtherImgs2 from "../img/products/otherProducts/other2.jpg";
-// import Toster1 from "../img/products/otherProducts/toster1.jpg";
-// import Toster2 from "../img/products/otherProducts/toster2.jpg";
-// import LampBlue1 from "../img/products/otherProducts/lampblue1.webp";
-// import LampBlue2 from "../img/products/otherProducts/lampblue2.webp";
-// import PulpUnit1 from "../img/products/otherProducts/pulpunit1.jpg";
-// import PulpUnit2 from "../img/products/otherProducts/pulpunit2.jpg";
-// import ModernGold1 from "../img/products/otherProducts/moderngold1.webp";
-// import ModernGold2 from "../img/products/otherProducts/moderngold2.webp";
-// import CleaningOil1 from "../img/products/otherProducts/cleaningoil1.jpg";
-// import CleaningOil2 from "../img/products/otherProducts/cleaningoil2.jpg";
-// import BlackAndWhite1 from "../img/products/otherProducts/blackandwhite1.webp";
-// import BlackAndWhite2 from "../img/products/otherProducts/blackandwhite2.webp";
-// import BodyOil1 from "../img/products/otherProducts/bodyoil1.jpg";
-// import BodyOil2 from "../img/products/otherProducts/bodyoil2.jpg";
-// import Shelf1 from "../img/products/otherProducts/shelf1.webp";
-// import Shelf2 from "../img/products/otherProducts/shelf2.webp";
-// import Vase1 from "../img/products/otherProducts/vase1.webp";
-// import Vase2 from "../img/products/otherProducts/vase2.webp";
-// import ChairBoom1 from "../img/products/otherProducts/chairboom1.webp";
-// import ChairBoom2 from "../img/products/otherProducts/chairboom2.webp";
-// import BlackLamp1 from "../img/products/otherProducts/blacklamp1.webp";
-// import BlackLamp2 from "../img/products/otherProducts/blacklamp2.jpg";
-// import GoldenLamp1 from "../img/products/otherProducts/goldenlamp1.webp";
-// import GoldenLamp2 from "../img/products/otherProducts/goldenlamp2.webp";
-// import Cotton1 from "../img/products/otherProducts/cotton1.jpg";
-// import Cotton2 from "../img/products/otherProducts/cotton2.jpg";
-// import WoodChair1 from "../img/products/otherProducts/woodchair1.webp";
-// import WoodChair2 from "../img/products/otherProducts/woodchair2.webp";
-// import Copenhagen1 from "../img/products/otherProducts/copenhagen1.webp";
-// import Copenhagen2 from "../img/products/otherProducts/copenhagen2.webp";
-// import Cam1 from "../img/products/otherProducts/cam1.webp";
-// import Cam2 from "../img/products/otherProducts/cam2.jpg";
-// import TableLamp1 from "../img/products/otherProducts/tablelamp1.webp";
-// import TableLamp2 from "../img/products/otherProducts/tablelamp2.webp";
-// import MatLamp1 from "../img/products/otherProducts/mat1.webp";
-// import MatLamp2 from "../img/products/otherProducts/mat2.webp";
-// import Comfy1 from "../img/products/otherProducts/comfy1.webp";
-// import Comfy2 from "../img/products/otherProducts/comfy2.webp";
+import Ceiling1 from "../assests/images/Ceiling/ceiling1.png";
+import Ceiling2 from "../assests/images/Ceiling/ceiling2.png";
+import Ceiling3 from "../assests/images/Ceiling/ceiling3.png";
+import Ceiling4 from "../assests/images/Ceiling/ceiling4.png";
 
 import Img1 from "../assests/images/Bg.webp";
 
@@ -65,7 +9,7 @@ export const items = [
   {
     id: 1,
     category: "chair",
-    img: Img1,
+    img: Ceiling1,
     description: "Little Armchair Sheepskin",
     price: 986,
     otherImgs: [],
@@ -78,7 +22,7 @@ export const items = [
   {
     id: 2,
     category: "kitchen",
-    img: Img1,
+    img: Ceiling2,
     description: "Pop-Up Toaster",
     price: 45,
     otherImgs: [],
@@ -91,7 +35,7 @@ export const items = [
   {
     id: 3,
     category: "lamp",
-    img: Img1,
+    img: Ceiling3,
     description: "Lamp Light Blue",
     price: 79,
     otherImgs: [],
@@ -104,7 +48,7 @@ export const items = [
   {
     id: 4,
     category: "furniture",
-    img: Img1,
+    img: Ceiling4,
     description: "Pulp Unit - 5 Compartments",
     price: 254,
     otherImgs: [],
