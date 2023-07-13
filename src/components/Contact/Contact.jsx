62f8f17b0283d660c144a00af319e3bde78ceb1b
Aman Sirohi <amansirohi077@gmail.com>
2023-07-13 19:40:20 +0530

    added messaging feature in contact form

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,8 +1,25 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./Contact.css";
 import Gif from "../../assests/images/contact.gif"
+import emailjs from "emailjs-com";
+import config from "../../Config/config";
 
 export default function Contact() {
+
+  const USER_ID = config.USER_ID;
+  const TEMPLATE_ID = config.TEMPLATE_ID;
+  const SERVICE_ID = config.SERVICE_ID;
+
+  const [submissionStatus, setSubmissionStatus] = useState(null);
+  useEffect(() => {
+    if (submissionStatus) {
+      const timer = setTimeout(() => {
+        setSubmissionStatus(null);
+      }, 5000);
+      return () => clearTimeout(timer);
+    }
+  }, [submissionStatus]);
+
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [mobile, setMobile] = useState("");
@@ -28,6 +45,14 @@ export default function Contact() {
   const handleMessageChange = (e) => {
     setMessage(e.target.value);
   };
+
+
+  const handleDismiss = () => {
+    setSubmissionStatus(null);
+  };
+
+
+
   const handleSubmit = (e) => {
     e.preventDefault();
      // Perform form validation
@@ -55,7 +80,44 @@ export default function Contact() {
       }
 
       setFormErrors(errors);
-    };
+
+      if (
+        !errors.name &&
+        !errors.email &&
+        !errors.mobile &&
+        !errors.message
+      ) {
+        // Create the email parameters
+        const emailParams = {
+          from_name: name,
+          from_email: email,
+          from_mobile: mobile,
+          message: message,
+        };
+ // Send the email using EmailJS
+ emailjs
+ .send(SERVICE_ID, TEMPLATE_ID, emailParams, USER_ID)
+ .then((response) => {
+   console.log("Email sent successfully!");
+   setSubmissionStatus("success");
+   // Reset the form after successful submission
+   setName("");
+   setEmail("");
+   setMobile("");
+   setMessage("");
+   setFormErrors({
+     name: false,
+     email: false,
+     mobile: false,
+     message: false,
+   });
+ })
+ .catch((error) => {
+   console.error("Email sending failed:", error);
+   setSubmissionStatus("error");
+ });
+}
+};
 
     const isValidEmail = (email) => {
       // Add your email validation logic here
@@ -128,7 +190,39 @@ export default function Contact() {
                 <span className="error-message">Please enter your message</span>
               )}
         </div>
+
+
+
         <button className="btnn" type="submit">Send</button>
+        {submissionStatus && (
+              <div
+                className={`submission-message ${
+                  submissionStatus === "success" ? "success" : "error"
+                } show`}
+              >
+                {submissionStatus === "success" ? (
+                  <>
+                    Your message has been sent successfully!
+                    <button
+                      className="dismiss-button"
+                      onClick={handleDismiss}
+                    >
+                      <i className="fas fa-times"></i>
+                    </button>
+                  </>
+                ) : (
+                  <>
+                    Sorry, an error occurred while sending your message. Please try again later.
+                    <button
+                      className="dismiss-button"
+                      onClick={handleDismiss}
+                    >
+                      <i className="fas fa-times"></i>
+                    </button>
+                  </>
+                )}
+              </div>
+            )}
       </form>
     </div>
   </div>
