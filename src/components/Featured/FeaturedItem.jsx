d5aeae996892593a34e7ba68d4ad79dffd0008e4
Aman Sirohi <amansirohi077@gmail.com>
2023-07-14 17:30:41 +0530

    added blur to featured item

--- src/components/Featured/FeaturedItem.jsx
+++ src/components/Featured/FeaturedItem.jsx
@@ -1,19 +1,18 @@
-import "./FeaturedSlider.css";
-import { items } from "../AllData";
-import { Link } from "react-router-dom";
+import './FeaturedSlider.css';
+import { items } from '../AllData';
+import { Link } from 'react-router-dom';
+import { LazyLoadImage } from 'react-lazy-load-image-component';
+import 'react-lazy-load-image-component/src/effects/blur.css';
 
 export default function FeaturedItem() {
-  const filteredItems = items.filter((item) => item.id >=1);
+  const filteredItems = items.filter((item) => item.id >= 1);
   return (
     <>
       {filteredItems.map((item) => (
         <div key={item.id} className="row-item">
-          <Link
-            onClick={() => window.top(0, 0)}
-            to={`/categories/product/${item.id}`}
-          >
+          <Link onClick={() => window.top(0, 0)} to={`/categories/product/${item.id}`}>
             <div className="item-header">
-              <img src={item.img} alt="product" />
+              <LazyLoadImage src={item.img} alt="product" effect="blur" />
             </div>
             <div className="item-description">
               <p>{item.description}</p>
@@ -25,4 +24,3 @@ export default function FeaturedItem() {
     </>
   );
 }
-
