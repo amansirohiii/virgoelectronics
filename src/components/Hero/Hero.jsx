e39ccfdcf2c32d13f87f9ebc2f37ddd653e7644a
Aman Sirohi <amansirohi077@gmail.com>
2023-07-14 12:41:48 +0530

    added bg image to hero

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,17 +1,22 @@
 import React from "react";
 import "./Hero.css";
+import Bg from "../../assests/images/bg.jpg";
 
 export default function Hero() {
+  const heroStyle = {
+    backgroundImage: `url(${Bg})`,
+    // opacity: 0.5,
+  };
+
   return (
-    <div className="hero-banner">
+    <div className="hero-banner" style={heroStyle}>
       <div className="hero-content">
         <h1 className="hero-heading">VIRGO ELECTRONICS</h1>
         <h2 className="hero-subheading">VIRGO fans - cool without compromise</h2>
-         <a href="/products/all"><button className="hero-button">Products</button></a>
-
+        <a href="/products/all">
+          <button className="hero-button">Products</button>
+        </a>
       </div>
     </div>
   );
 }
-
-
