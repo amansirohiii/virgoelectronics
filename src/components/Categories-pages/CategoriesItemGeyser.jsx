2b34396b997085bd14b43fbf372eafd66a7c9aa4
Aman Sirohi <amansirohi077@gmail.com>
2023-07-14 16:03:42 +0530

    added lazy load to images

--- src/components/Categories-pages/CategoriesItemGeyser.jsx
+++ src/components/Categories-pages/CategoriesItemGeyser.jsx
@@ -1,8 +1,12 @@
+import React from "react";
 import { Link } from "react-router-dom";
 import { items } from "../AllData";
+import { LazyLoadImage } from "react-lazy-load-image-component";
+import "react-lazy-load-image-component/src/effects/blur.css";
 
 function CategoriesItem() {
   const filteredItems = items.filter((item) => item.category === "geyser");
+
   return (
     <>
       <div className="proud-container">
@@ -10,10 +14,14 @@ function CategoriesItem() {
           <div className="products-grid">
             {filteredItems.map((item) => (
               <div key={item.id} className="product normal">
-                <Link onClick={() => window.top(0, 0)}
-                to={`/categories/product/${item.id}`}>
+                <Link onClick={() => window.top(0, 0)} to={`/categories/product/${item.id}`}>
                   <div className="product-header">
-                    <img src={item.img} alt="product1" />
+                    <div className="image-placeholder" style={{ height: "100%" }} />
+                    <LazyLoadImage
+                      src={item.img}
+                      alt="product1"
+                      effect="blur"
+                    />
                   </div>
                   <div className="product-details">
                     <p>{item.description}</p>
--- src/components/CategoriesItem.jsx
+++ src/components/CategoriesItem.jsx
@@ -1,34 +1,67 @@
+import React, { useEffect, useRef } from "react";
 import { Link } from "react-router-dom";
 import { items } from "./AllData";
+import { LazyLoadImage } from "react-lazy-load-image-component";
+import "react-lazy-load-image-component/src/effects/blur.css";
+
 
 function CategoriesItem() {
-  return (
+  const containerRef = useRef(null);
 
-    <>
-      <div className="proud-container">
-        <div className="container">
-          <div className="products-grid">
-            {items.map((item) => (
-              <div key={item.id} className="product normal">
-                <Link
-                  onClick={() => window.top(0,0)}
-                  to={`/categories/product/${item.id}`}
-                >
-                  <div className="product-header">
-                    <img src={item.img} alt="product1" />
-                  </div>
-                  <div className="product-details">
-                    <p>{item.description}</p>
-                    {/* <p className="item-price">{item.price}$</p> */}
-                  </div>
-                </Link>
-              </div>
-            ))}
-          </div>
+  useEffect(() => {
+    const containerElement = containerRef.current;
+
+    const observer = new IntersectionObserver(
+      (entries) => {
+        entries.forEach((entry) => {
+          if (entry.isIntersecting) {
+            const lazyImages = entry.target.querySelectorAll(".lazy-load");
+            lazyImages.forEach((lazyImage) => {
+              lazyImage.src = lazyImage.dataset.src;
+              lazyImage.classList.remove("lazy-load");
+            });
+            observer.unobserve(entry.target);
+          }
+        });
+      },
+      { rootMargin: "200px" } // Adjust the rootMargin value as per your preference
+    );
+
+    observer.observe(containerElement);
+
+    return () => {
+      observer.disconnect();
+    };
+  }, []);
+
+  return (
+    <div className="proud-container">
+      <div className="container" ref={containerRef}>
+        <div className="products-grid">
+          {items.map((item) => (
+            <div key={item.id} className="product normal">
+              <Link onClick={() => window.top(0, 0)} to={`/categories/product/${item.id}`}>
+                <div className="product-header">
+                  <LazyLoadImage
+                    src={item.img}
+                    alt="product1"
+                    effect="blur"
+                    className="lazy-load"
+                    data-src={item.img}
+                  />
+                </div>
+                <div className="product-details">
+                  <p>{item.description}</p>
+                  {/* <p className="item-price">{item.price}$</p> */}
+                </div>
+              </Link>
+            </div>
+          ))}
         </div>
       </div>
-    </>
+    </div>
   );
 }
+
 
 export default CategoriesItem;
